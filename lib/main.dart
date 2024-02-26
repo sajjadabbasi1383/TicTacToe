870b1932f669d97b95e32cd6f62f806efef4d3e2
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-02-26 16:22:21 +0330

    fix filled all box and not winner

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class _HomePageState extends State<HomePage> {
   static const myTextStyle = TextStyle(fontSize: 25, color: Colors.white);
 
   bool ohTurn = true;
+  bool finish=false;
   List<String> displayExOh = [
     '',
     '',
@@ -41,6 +42,7 @@ class _HomePageState extends State<HomePage> {
 
   int ohScore = 0;
   int exScore = 0;
+  int filledBoxes=0;
 
   @override
   Widget build(BuildContext context) {
@@ -125,8 +127,10 @@ class _HomePageState extends State<HomePage> {
     setState(() {
       if (ohTurn&&displayExOh[index]=='') {
         displayExOh[index] = 'o';
+        filledBoxes+=1;
       } else if(!ohTurn&&displayExOh[index]=='') {
         displayExOh[index] = 'x';
+        filledBoxes+=1;
       }
       ohTurn = !ohTurn;
       _checkWinner();
@@ -138,66 +142,79 @@ class _HomePageState extends State<HomePage> {
     if (displayExOh[0] == displayExOh[1] &&
         displayExOh[0] == displayExOh[2] &&
         displayExOh[0] != '') {
-      _showDialog(displayExOh[0]);
+      finish=true;
+      _showWinDialog(displayExOh[0]);
     }
 
     //check 2st row
     if (displayExOh[3] == displayExOh[4] &&
         displayExOh[3] == displayExOh[5] &&
         displayExOh[3] != '') {
-      _showDialog(displayExOh[3]);
+      finish=true;
+      _showWinDialog(displayExOh[3]);
     }
 
     //check 3st row
     if (displayExOh[6] == displayExOh[7] &&
         displayExOh[6] == displayExOh[8] &&
         displayExOh[6] != '') {
-      _showDialog(displayExOh[6]);
+      finish=true;
+      _showWinDialog(displayExOh[6]);
     }
 
     //check 1st column
     if (displayExOh[0] == displayExOh[3] &&
         displayExOh[0] == displayExOh[6] &&
         displayExOh[0] != '') {
-      _showDialog(displayExOh[0]);
+      finish=true;
+      _showWinDialog(displayExOh[0]);
     }
 
     //check 2st column
     if (displayExOh[1] == displayExOh[4] &&
         displayExOh[1] == displayExOh[7] &&
         displayExOh[1] != '') {
-      _showDialog(displayExOh[1]);
+      finish=true;
+      _showWinDialog(displayExOh[1]);
     }
 
     //check 3st column
     if (displayExOh[2] == displayExOh[5] &&
         displayExOh[2] == displayExOh[8] &&
         displayExOh[2] != '') {
-      _showDialog(displayExOh[2]);
+      finish=true;
+      _showWinDialog(displayExOh[2]);
     }
 
     //check diagonal
     if (displayExOh[0] == displayExOh[4] &&
         displayExOh[0] == displayExOh[8] &&
         displayExOh[0] != '') {
-      _showDialog(displayExOh[0]);
+      finish=true;
+      _showWinDialog(displayExOh[0]);
     }
 
     //check diagonal
     if (displayExOh[2] == displayExOh[4] &&
         displayExOh[2] == displayExOh[6] &&
         displayExOh[2] != '') {
-      _showDialog(displayExOh[2]);
+      finish=true;
+      _showWinDialog(displayExOh[2]);
     }
+
+    else if(filledBoxes==9&&finish==false){
+      _showDrawDialog();
+    }
+
   }
 
-  void _showDialog(String winner) {
+  void _showWinDialog(String winner) {
     showDialog(
       barrierDismissible: false,
       context: context,
       builder: (context) {
         return AlertDialog(
-          backgroundColor: Colors.grey[300],
+          backgroundColor: Colors.grey[100],
           title: Text('Winner is : $winner'),
           actions: [
             TextButton(
@@ -220,11 +237,36 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  void _showDrawDialog() {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Colors.grey[100],
+          title: const Text('Draw!'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  _clearBoard();
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  "Play Again!",
+                  style: TextStyle(color: Colors.grey[900]),
+                ))
+          ],
+        );
+      },
+    );
+  }
+
   void _clearBoard(){
     setState(() {
       for(int i=0;i<9;i++){
         displayExOh[i]='';
       }
     });
+    filledBoxes=0;
   }
 }
