dc6ecca96c88f9b3a89b5f0b696cd71bb722ea30
sajjadabbasi1383 <sajjadabbas0083@gmail.com>
2024-02-28 12:51:16 +0330

    Intro Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'package:avatar_glow/avatar_glow.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,38 +14,37 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: IntroScreen(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class IntroScreen extends StatefulWidget {
+  const IntroScreen({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<IntroScreen> createState() => _IntroScreenState();
 }
 
-class _HomePageState extends State<HomePage> {
-  static const myTextStyle = TextStyle(fontSize: 25, color: Colors.white);
-
-  bool ohTurn = true;
-  bool winner = false;
-  List<String> displayExOh = [
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-    '',
-  ];
-
-  int ohScore = 0;
-  int exScore = 0;
-  int filledBoxes = 0;
+class _IntroScreenState extends State<IntroScreen> {
+  static const myTextStyle = TextStyle(
+    fontSize: 17,
+    color: Colors.white,
+    fontFamily: 'PressStart2P',
+    letterSpacing: 2,
+  );
+  static const myTextStyle3 = TextStyle(
+    fontSize: 23,
+    color: Colors.white,
+    fontFamily: 'PressStart2P',
+    letterSpacing: 2,
+  );
+  static const myTextStyle2 = TextStyle(
+    fontSize: 15,
+    color: Colors.black,
+    fontFamily: 'PressStart2P',
+    letterSpacing: 2,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -112,177 +114,13 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ),
                     ),
-                  );
-                },
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(child: Container())
-        ],
+          ],
+        ),
       ),
     ));
-  }
-
-  void _tapped(int index) {
-    setState(() {
-      if (ohTurn && displayExOh[index] == '') {
-        displayExOh[index] = 'o';
-        filledBoxes += 1;
-      } else if (!ohTurn && displayExOh[index] == '') {
-        displayExOh[index] = 'x';
-        filledBoxes += 1;
-      }
-      ohTurn = !ohTurn;
-      _checkWinner();
-    });
-  }
-
-  void _checkWinner() {
-    //check 1st row
-    if (displayExOh[0] == displayExOh[1] &&
-        displayExOh[0] == displayExOh[2] &&
-        displayExOh[0] != '') {
-      if (filledBoxes == 9) {
-        winner = true;
-      }
-      _showWinDialog(displayExOh[0]);
-    }
-
-    //check 2st row
-    if (displayExOh[3] == displayExOh[4] &&
-        displayExOh[3] == displayExOh[5] &&
-        displayExOh[3] != '') {
-      if (filledBoxes == 9) {
-        winner = true;
-      }
-      _showWinDialog(displayExOh[3]);
-    }
-
-    //check 3st row
-    if (displayExOh[6] == displayExOh[7] &&
-        displayExOh[6] == displayExOh[8] &&
-        displayExOh[6] != '') {
-      if (filledBoxes == 9) {
-        winner = true;
-      }
-      _showWinDialog(displayExOh[6]);
-    }
-
-    //check 1st column
-    if (displayExOh[0] == displayExOh[3] &&
-        displayExOh[0] == displayExOh[6] &&
-        displayExOh[0] != '') {
-      if (filledBoxes == 9) {
-        winner = true;
-      }
-      _showWinDialog(displayExOh[0]);
-    }
-
-    //check 2st column
-    if (displayExOh[1] == displayExOh[4] &&
-        displayExOh[1] == displayExOh[7] &&
-        displayExOh[1] != '') {
-      if (filledBoxes == 9) {
-        winner = true;
-      }
-      _showWinDialog(displayExOh[1]);
-    }
-
-    //check 3st column
-    if (displayExOh[2] == displayExOh[5] &&
-        displayExOh[2] == displayExOh[8] &&
-        displayExOh[2] != '') {
-      if (filledBoxes == 9) {
-        winner = true;
-      }
-      _showWinDialog(displayExOh[2]);
-    }
-
-    //check diagonal
-    if (displayExOh[0] == displayExOh[4] &&
-        displayExOh[0] == displayExOh[8] &&
-        displayExOh[0] != '') {
-      if (filledBoxes == 9) {
-        winner = true;
-      }
-      _showWinDialog(displayExOh[0]);
-    }
-
-    //check diagonal
-    if (displayExOh[2] == displayExOh[4] &&
-        displayExOh[2] == displayExOh[6] &&
-        displayExOh[2] != '') {
-      if (filledBoxes == 9) {
-        winner = true;
-      }
-      _showWinDialog(displayExOh[2]);
-    }
-
-    if (winner == false && filledBoxes == 9) {
-      _showDrawDialog();
-    }
-  }
-
-  void _showWinDialog(String winner) {
-    showDialog(
-      barrierDismissible: false,
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          backgroundColor: Colors.grey[100],
-          title: Text('Winner is : $winner'),
-          actions: [
-            TextButton(
-                onPressed: () {
-                  _clearBoard();
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  "Play Again!",
-                  style: TextStyle(color: Colors.grey[900]),
-                ))
-          ],
-        );
-      },
-    );
-    if (winner == 'o') {
-      ohScore += 1;
-    } else if (winner == 'x') {
-      exScore += 1;
-    }
-  }
-
-  void _showDrawDialog() {
-    showDialog(
-      barrierDismissible: false,
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          backgroundColor: Colors.grey[100],
-          title: const Text('Draw!'),
-          actions: [
-            TextButton(
-                onPressed: () {
-                  _clearBoard();
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  "Play Again!",
-                  style: TextStyle(color: Colors.grey[900]),
-                ))
-          ],
-        );
-      },
-    );
-  }
-
-  void _clearBoard() {
-    setState(() {
-      for (int i = 0; i < 9; i++) {
-        displayExOh[i] = '';
-      }
-    });
-    filledBoxes = 0;
-    winner = false;
   }
 }
